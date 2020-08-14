cdc917a8eb72ea528302bc724a60a7fb10ea8055
Elvis Lumnica <58716184+elumnica@users.noreply.github.com>
2020-08-13 22:46:42 -0400

    second solution

--- nthFib.js
+++ nthFib.js
@@ -24,3 +24,16 @@ function getNthFib(n){
         return getNthFib(n - 1) + getNthFib(n - 2);
     }
 }
+
+
+//Second Solution
+
+function get NthFib(n, memoize = {1: 0, 2: 1}){
+    if (n in memoize){
+        return memoize(n);
+    }
+    else{
+        memoize[n] = getNthFib(n - 1, memoize) + getNthFib(n - 2, memoize);
+        return memoize[n];
+    }
+}

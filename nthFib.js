7770e99058cac7b2bed2b2a9e35d681d81507466
Elvis Lumnica <58716184+elumnica@users.noreply.github.com>
2020-08-13 22:34:14 -0400

    optimal solution

---
+++ nthFib.js
@@ -0,0 +1,24 @@
+/*
+Prompt: 
+
+The Fibonacci sequence as follows: the first number of the
+sequence is zero, the second number is 1, and the nth number is the dum of the (n-1)th
+and (n-2)th numbers. Write a function that takes in an integer n and returns the nth Fibonacci
+number.
+
+*/
+
+
+function getNthFib(n){
+    if (n===2){
+        return 1;
+    }
+
+    else if (n === 1){
+        return 0;
+    }
+
+    else{
+        return getNthFib(n - 1) + getNthFib(n - 2);
+    }
+}
